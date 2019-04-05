8e212dace2d13d88cdf737768a60844a386960a8
Mkomarr <48152102+Mkomarr@users.noreply.github.com>
2019-04-05 14:53:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -32,17 +32,35 @@ const icon = document.querySelector('.dropdownIcon');
 const menu = document.querySelector('.menu')
 const hideMenu = document.querySelectorAll('.desktopMenu')
 
-icon.addEventListener('click', function() {
+icon.addEventListener('click', () => {
 	if (menu.className === 'menu') {
 		menu.className = 'mobileMenu';
 	} else {
 		menu.className = 'menu';
 	}	
 });
-//na kiedyś - jak zmienić archit, żeby działało tylko na wersji mob
+//na kiedyś - co zmienić (archit?), żeby działało tylko na wersji mob
 
 /*------- schowaj menu po kliknięciu w inny element niż hamburger menu ----------*/
 
 hideMenu.forEach(singleLi => singleLi.addEventListener('click', () => {
 		menu.className = 'menu';
 }));
+
+/*--------------------------licznik dni-----------------------------*/
+
+const endTime = new Date('2019-05-06 22:34:00').getTime();
+const counter = document.querySelector('.countdown');
+const countdown = () => {
+	const nowTime = new Date().getTime();
+	const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
+	counter.textContent = days;
+	//console.log('działa')
+}
+window.onload = countdown();
+
+/*---------------------pokaż cień pod menu gdy scroll--------------*/
+
+const addShadow = () => {
+
+}
